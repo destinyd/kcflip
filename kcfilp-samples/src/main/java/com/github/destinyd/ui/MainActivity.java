d1e7cbb4acaa4d5cf2573dfed3829182b2c1d26b
destinyd <destinyd_war@163.com>
2014-06-05 11:21:17 +0800

    main activity to list

--- kcfilp-samples/src/main/java/com/github/destinyd/ui/MainActivity.java
+++ kcfilp-samples/src/main/java/com/github/destinyd/ui/MainActivity.java
@@ -1,65 +1,68 @@
 package com.github.destinyd.ui;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Gravity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.Menu;
-
 import android.view.MenuItem;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import com.github.destinyd.KCFlip;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import com.github.destinyd.R;
 
-public class MainActivity extends Activity {
-    KCFlip kcFlip;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        kcFlip = new KCFlip(this);
-        TextView textView = new TextView(this);
-        textView.setText("测试文字1");
-        textView.setGravity(Gravity.CENTER);
-        textView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
-        TextView textView1 = new TextView(this);
-        textView1.setText("测试文字2");
-        textView1.setGravity(Gravity.CENTER);
-        textView1.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
-        kcFlip.setViewBrief(textView);
-        kcFlip.setViewDetail(textView1);
-
-        addContentView(kcFlip, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        setListAdapter(
+                new SimpleAdapter(
+                        this, getData(), android.R.layout.simple_list_item_1, new String[]{"title"},
+                        new int[]{android.R.id.text1}
+                )
+        );
+        getListView().setScrollbarFadingEnabled(false);
     }
 
+    @SuppressWarnings("unchecked")
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-
-        return true;
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Map<String, Object> map = (Map<String, Object>) l.getItemAtPosition(position);
+        Intent intent = new Intent(this, (Class<? extends Activity>) map.get("activity"));
+        startActivity(intent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // TODO Auto-generated method stub
-        if (item.getItemId() == R.id.action_to_brief) {
-            kcFlip.to_brief();
-        }
-        return super.onContextItemSelected(item);
+    private List<? extends Map<String, ?>> getData() {
+        List<Map<String, Object>> data = new ArrayList<Map<String, Object>>();
+        addItem(data, "TextViews", FlipTextViewActivity.class);
+//        addItem(data, "Buttons", FlipButtonActivity.class);
+//        addItem(data, "Complex Layouts", FlipComplexLayoutActivity.class);
+//        addItem(data, "Async Content", FlipAsyncContentActivity.class);
+//        addItem(data, "Event Listener", FlipTextViewAltActivity.class);
+//        addItem(data, "Horizontal", FlipHorizontalLayoutActivity.class);
+//        addItem(data, "Issue #5", Issue5Activity.class);
+//        addItem(data, "XML Configuration", FlipTextViewXmlActivity.class);
+//        addItem(data, "Fragment", FlipFragmentActivity.class);
+//        addItem(data, "Dynamic Adapter Size", FlipDynamicAdapterActivity.class);
+//        addItem(data, "WebView", FlipWebViewActivity.class);
+//        addItem(data, "Delete page", FlipDeleteAdapterActivity.class);
+//        addItem(data, "Issue #51", Issue51Activity.class);
+
+        return data;
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        kcFlip.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        kcFlip.onPause();
+    private void addItem(List<Map<String, Object>> data, String title,
+                         Class<? extends Activity> activityClass) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("title", data.size() + ". " + title);
+        map.put("activity", activityClass);
+        data.add(map);
     }
 }
